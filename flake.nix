8f9b208998488995e07c453c4651d1b8af50ba9a
Lucio Franco <luciofranco14@gmail.com>
2025-04-16 20:53:19 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    nix-github-actions = {
+      url = "github:nix-community/nix-github-actions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nix-github-actions, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        packages.default = pkgs.hello;
+
+        devShells.default = pkgs.mkShell {
+          name = "vim-config";
+
+          nativeBuildInputs = with pkgs; [ nixd nixfmt-classic ];
+
+        };
+      };
+      flake = {
+        githubActions =
+          nix-github-actions.lib.mkGithubMatrix { checks = self.packages; };
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
