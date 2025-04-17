34b9ea9628d393f4da17a64b912269eb5dcc2f8e
Lucio Franco <luciofranco14@gmail.com>
2025-04-16 21:21:09 -0400

    add gitignore

--- flake.nix
+++ flake.nix
@@ -5,40 +5,90 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
     nix-github-actions = {
       url = "github:nix-community/nix-github-actions";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-github-actions, flake-parts, ... }:
+  outputs =
+    inputs@{
+      self,
+      nix-github-actions,
+      flake-parts,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
-
+        inputs.git-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
       ];
-      systems =
-        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        packages.default = pkgs.hello;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          packages.default = pkgs.hello;
 
-        devShells.default = pkgs.mkShell {
-          name = "vim-config";
+          devShells.default = pkgs.mkShell {
+            name = "vim-config";
 
-          nativeBuildInputs = with pkgs; [ nixd nixfmt-classic ];
+            nativeBuildInputs =
+              with pkgs;
+              [
+                nixd
+                statix
+                config.treefmt.build.wrapper
+              ]
+              ++ (lib.attrValues config.treefmt.build.programs);
 
+            shellHook = ''
+              ${config.pre-commit.installationScript}
+            '';
+          };
+
+          pre-commit = {
+            check.enable = true;
+            settings.hooks = {
+              actionlint.enable = true;
+              statix.enable = true;
+              treefmt.enable = true;
+            };
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            flakeCheck = false; # Covered by git-hooks check
+            programs = {
+              nixfmt.enable = true;
+            };
+          };
         };
-      };
       flake = {
-        githubActions =
-          nix-github-actions.lib.mkGithubMatrix { checks = self.packages; };
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-
+        githubActions = nix-github-actions.lib.mkGithubMatrix { checks = self.packages; };
       };
     };
 }
